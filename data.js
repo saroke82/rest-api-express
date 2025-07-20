8b061851b84877e69c3cace69b27babcf25ffd69
saroke82 <saroke82@gmail.com>
2025-07-20 17:02:57 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+let items = [
+  { id: 1, name: 'Item One', description: 'First item description' },
+  { id: 2, name: 'Item Two', description: 'Second item description' }
+];
+
+module.exports = items;
